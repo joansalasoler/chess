9285b95ca579336d9fc78a7a9a39e9cf52971600
Joan Sala <contact@joansala.com>
2024-07-04 11:20:49 +0200

    Command line option to set default hash table size

--- src/main/java/com/joansala/game/chess/ChessModule.java
+++ src/main/java/com/joansala/game/chess/ChessModule.java
@@ -65,6 +65,12 @@ public class ChessModule extends BaseModule {
           description = "Openings book root threshold"
         )
         private static double threshold = ROOT_THRESHOLD;
+
+        @Option(
+          names = "--cache-size",
+          description = "Default hash table size (bytes)"
+        )
+        private static long cacheSize = GameCache.DEFAULT_SIZE;
     }
 
 
@@ -75,7 +81,15 @@ public class ChessModule extends BaseModule {
         bind(Game.class).to(ChessGame.class);
         bind(Board.class).to(ChessBoard.class);
         bind(Engine.class).to(Negamax.class);
-        bind(Cache.class).to(GameCache.class);
+    }
+
+
+    /**
+     * Transpositions table provider.
+     */
+    @Provides @Singleton @SuppressWarnings("rawtypes")
+    public static Cache provideCache() {
+        return new GameCache(ChessCommand.cacheSize);
     }
 
 
